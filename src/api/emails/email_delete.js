03640c3a97f75f8b1cbad9327ad0a90bcef2d1f8
VictorRaffaele <victorraffaele@gmail.com>
2025-03-27 15:43:28 -0300

    feat: delete email

---
+++ src/api/emails/email_delete.js
@@ -0,0 +1,27 @@
+const { query } = require('../../config/database');
+const { validateData } = require('./email_validations');
+
+async function deleteEmail(req, res) {
+    const funcTag = '[deleteEmail]';
+    try {
+        const { email_id } = req.body;
+        const valid = validateData('delete', null, email_id)
+        if (valid) {
+            throw valid;
+        }
+
+        console.log(`${funcTag} Deleting email data in emails table`);
+        textQuery = `
+            DELETE FROM emails
+            WHERE email_id = $1
+            RETURNING email_id;
+        `;
+        const emailData = await query(textQuery, [email_id]);
+        console.log(`${funcTag} Email deleted`);
+        res.status(200).json({ data: emailData.rows[0], message: 'Email deleted', error: null });
+    } catch (error) {
+        console.error(`${funcTag} Email deletion failed: ${error.message}`);
+        res.status(400).json({ data: null, message: 'An error has occurred', error: error.message });
+    }
+}
+module.exports = { deleteEmail };
--- src/api/routes.js
+++ src/api/routes.js
@@ -2,10 +2,12 @@ const routes = require('express').Router();
 const { createEmail } = require('./emails/email_create');
 const { listEmail } = require('./emails/email_list');
 const { updateEmail } = require('./emails/email_update');
+const { deleteEmail } = require('./emails/email_delete');
 
 // Email routes
 routes.post('/emails/create', createEmail);
 routes.get('/emails/list', listEmail);
 routes.put('/emails/update', updateEmail);
+routes.delete('/emails/delete', deleteEmail);
 
 module.exports = { routes };
