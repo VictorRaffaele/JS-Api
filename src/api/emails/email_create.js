a376abf109c000c26b63642b43525b4610f7b044
VictorRaffaele <victorraffaele@gmail.com>
2025-03-27 15:20:59 -0300

    feat: remove validations from create

--- src/api/emails/email_create.js
+++ src/api/emails/email_create.js
@@ -1,13 +1,13 @@
 const crypto = require('crypto');
 const { query } = require('../../config/database');
-const { regexEmail } = require('../api_utils');
+const { validateData, emailExists } = require('./email_validations');
 
 async function createEmail(req, res) {
     const funcTag = '[createEmail]';
     try {
         const { email } = req.body;
 
-        const valid = validateData(email)
+        const valid = validateData('create', email, null)
         if (valid) {
             throw valid;
         }
@@ -30,42 +30,6 @@ async function createEmail(req, res) {
         console.error(`${funcTag} Email creation failed: ${error.message}`);
         res.status(400).json({ data: null, message: 'An error has occurred', error: error.message });
     }
-}
-
-function validateData(email) {
-    const funcTag = '[validateData]';
-    try {
-        console.log(`${funcTag} Validating email data`);
-
-        if (!email) {
-            console.log(`${funcTag} No email received`);
-            throw new Error('Email is required');
-        }
-        if (!regexEmail(email)) {
-            console.log(`${funcTag} Email has invalid format`);
-            throw new Error('Email is invalid');
-        }
-        return null;
-    } catch (error) {
-        console.error(`${funcTag} Error validating data: ${error.message}`);
-        return error;
-    }
-}
-
-async function emailExists(email) {
-  const funcTag = '[emailExists]';
-  try {
-      console.log(`${funcTag} Checking if email exists in emails table`);
-      const user = await query(`SELECT EXISTS (SELECT true FROM emails WHERE email = $1);`, [email]);
-      if (user.rows[0].exists) {
-          console.log(`${funcTag} Email already exists`);
-          throw new Error('Email already exists');
-      }
-      return null;
-  } catch (error) {
-      console.error(`${funcTag} Error checking if email exist: ${error.message}`);
-      return error;
-  }
 }
 
 module.exports = { createEmail };
