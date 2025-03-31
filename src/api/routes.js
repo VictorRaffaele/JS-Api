96613f88fecfd8ea4c478ae00ee1c223d4ad253c
VictorRaffaele <victorraffaele@gmail.com>
2025-03-31 17:16:58 -0300

    feat: create users

--- src/api/routes.js
+++ src/api/routes.js
@@ -3,11 +3,15 @@ const { createEmail } = require('./emails/email_create');
 const { listEmail } = require('./emails/email_list');
 const { updateEmail } = require('./emails/email_update');
 const { deleteEmail } = require('./emails/email_delete');
+const { userCreate } = require('./user/user_create');
 
 // Email routes
 routes.post('/emails/create', createEmail);
 routes.get('/emails/list', listEmail);
 routes.put('/emails/update', updateEmail);
 routes.delete('/emails/delete', deleteEmail);
+
+// User routes
+routes.post('/users/create', userCreate);
 
 module.exports = { routes };
---
+++ src/api/user/user_create.js
@@ -0,0 +1,46 @@
+const { query } = require('../../config/database');
+const { validateData, nameExists } = require('./user_validations');
+const crypto = require('crypto');
+
+async function userCreate (req, res) {
+    const funcTag = '[userCreate]';
+    try {
+        console.log(`${funcTag} Inserting user data in users table`);
+        const { username, email, password } = req.body;
+
+        const invalid = await validateData('create', username, password);
+        if (invalid) {
+            throw invalid;
+        }
+
+        const exists = await nameExists(username);
+        if (exists) {
+            throw exists;
+        }
+
+        const resp = await fetch(
+            'http://localhost:3000/api/emails/create', {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ email: email }),
+        });
+        const responseData = await resp.json();
+
+        if (!resp.ok) {
+            console.error(`${funcTag} Email creation failed: ${responseData.error}`);
+            throw new Error(responseData.error);
+        }
+        const userQuery = `
+            INSERT INTO users (user_id, username, email, password)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *;
+        `;
+        const user = await query(userQuery, [crypto.randomUUID(), username, responseData.data.email_id, password]);
+        res.status(201).json( { data: user.rows[0], message: 'User created', error: null } );
+    } catch (error) {
+        console.error(`${funcTag} User creation failed: ${error.message}`);
+        res.status(400).json({ data: null, message: 'An error has occurred', error: error.message });
+    }
+}
+
+module.exports = { userCreate };
