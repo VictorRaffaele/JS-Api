eda775e8a5500de55037e6d7e73752358ddc1b38
VictorRaffaele <victorraffaele@gmail.com>
2025-04-02 16:39:22 -0300

    feat: get email id by user id

--- src/api/user/user_list.js
+++ src/api/user/user_list.js
@@ -18,4 +18,21 @@ async function userList(req, res) {
     }
 }
 
-module.exports = { userList };
+async function getEmailIdbyUserId(req, res) {
+    const funcTag = '[getEmailIdbyUser]';
+    try {
+        console.log(`${funcTag} Getting email ID`);
+        const { user_id } = req.params;
+        const emailId = await query(`SELECT email FROM users WHERE user_id = $1`, [user_id]);
+        if (emailId.rows.length === 0) {
+            return res.status(404).json({ data: null, message: 'Email not found', error: null });
+        }
+        console.log(`${funcTag} Email ID retrieved successfully`);
+        res.status(200).json({ data: emailId.rows[0], message: 'Email ID retrieved', error: null });
+    } catch (error) {
+        console.error(`${funcTag} Error getting email ID: ${error.message}`);
+        res.status(400).json({ data: null, message: 'An error has occurred', error: error.message });
+    }   
+}
+
+module.exports = { userList, getEmailIdbyUserId };
